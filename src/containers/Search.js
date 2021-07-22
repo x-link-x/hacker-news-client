23e2922de32426ae0b2e4e0af3923b8a0ebd2136
Link <xlinkx_@outlook.com>
2021-07-22 12:47:36 +0100

    initial commit

---
+++ src/containers/Search.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Search = () => {
+    return (
+        <>
+            <p>Search page</p>
+            <SearchBar/>
+            <SearchResults/>
+        </>
+    )
+}
+
+export default Search;
